a1b3136500e641d7e0ae1e6d50692869f2ba5260
prabha <Prabhakaran.mails@gmail.com>
2020-09-18 22:18:34 -0400

    Added the docsting for the gbq functions

--- gbq_data_history/gbq_helper_functions.py
+++ gbq_data_history/gbq_helper_functions.py
@@ -4,6 +4,7 @@ from google.cloud import bigquery
 
 def get_id_arrest_updatedon():
     """
+    Gets the id,arrest,updated_on & is_active info for all the past crime records from GBQ
     """
     bq_client = bigquery.Client()
     query = """
@@ -19,9 +20,13 @@ def get_id_arrest_updatedon():
 
 def insert_to_bigquery(rows_to_insert,dataset,table):
     """
+    Inserts the rows into GBQ
+    
+    Args:
+        rows_to_insert(list of tuples): Rows to insert
+        dataset(string): GBQ dataset which has the table that we want to insert
+        table(string): table to insert the records
     """
-
-           
     bq_client = bigquery.Client()
     dataset_ref = bq_client.dataset(dataset)
     table_ref=dataset_ref.table(table)
@@ -35,11 +40,15 @@ def insert_to_bigquery(rows_to_insert,dataset,table):
     
 def set_rows_inactive(inactive_rows):
     """
+    Marks the rows as inactive, by setting the column is_active to False
+    
+    Args:
+        inactive_rows(list of integers): case ids which needs to marked as inactive
     """
     inactive_rows_string = ','.join(map(str,inactive_rows))
     bq_client = bigquery.Client()
     query = "update `chicago-crime-284514.crime_dataset.chicago_crime1` set is_active = False where id in ("+inactive_rows_string+")"
-    print(query)
+    print("Starting the GBQ job to update the flag to inactive")
     query_job = bq_client.query(query).result()
-    print(query_job)
+    print("GBQ job to update the flag to inactive is complete")
     
