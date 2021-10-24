fb695bda045c1d9307de60b8fcb13c0c3f7194c0
prabha <Prabhakaran.mails@gmail.com>
2021-10-24 11:49:34 -0400

    removed the commented code

--- datalake/data_pipeline/batch/pipeline.py
+++ datalake/data_pipeline/batch/pipeline.py
@@ -9,16 +9,9 @@ import argparse
 
 import logging
 logging.info(" Pipeline Started...")
-# For My MacBook
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/Users/prabha/GitHub/chicago_crime/beam/chicago-crime-batch-processing.json"
 
-# For my windows Laptop
-#os.environ['GOOGLE_APPLICATION_CREDENTIALS']=r"C:\Users\arivalagan.prabhakar\Documents\Github\chicago_crime\beam\data_pipeline\batch\chicago-crime-batch-processing.json"
-#bq_client = bigquery.Client()
-#table_schema= 'id:numeric,case_number:string,date:datetime,block:string,iucr:string,primary_type:string,description:string,location_description:string,arrest:boolean,domestic:boolean,beat:string,district:string,ward:string,community_area:string,fbi_code:string,x_coordinate:numeric,y_coordinate:numeric,year:numeric,updated_on:datetime,latitude:float,longitude:float,location:string'
-#table_schema = bq_client.schema_from_json('table_schema.json')
-# parser = argparse.ArgumentParser()
-# path_args,pipeline_args = parser.parse_known_args()
+# Service Account Key
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/Users/prabha/GitHub/chicago_crime/beam/chicago-crime-batch-processing.json"
 
 
 
@@ -29,6 +22,9 @@ class chicago_crime_options(PipelineOptions):
 
 class fetch_data(beam.DoFn):
     def process(self,url):
+        """
+        Pulls the data using API call , it pulls the records that are updated in the last 3 days.
+        """
         updated_last_n_days = 3
         client = Socrata(url, app_token="Ttz4HIh52J3g53HKTYKMNxu4M")
 
@@ -44,6 +40,9 @@ class fetch_data(beam.DoFn):
 
 class data_type_conversion(beam.DoFn):
     def process(self,row):
+        """
+        Converts the datatype for numeric columns
+        """
         int_type = ['id','x_coordinate','y_coordinate']
         float_type = ['latitude','longitude']
 
@@ -62,13 +61,11 @@ class data_type_conversion(beam.DoFn):
     
         if 'location' in row:
             row['location']['human_address'] = json.loads(row['location']['human_address'])
-            #row['location']={}
         else:
             row['location']={}
          
         row['db_updated'] = datetime.now()
         
-
         return [row]
     
 
@@ -77,18 +74,17 @@ options = PipelineOptions()
 with beam.Pipeline(options=options) as p:
     input_options = options.view_as(chicago_crime_options)
     
+    # Fetching the data
     data    =   (p 
                 | 'URL ' >> beam.Create(['data.cityofchicago.org'])
                 | 'Call API' >> beam.ParDo(fetch_data())
     )
     
-    
+    # Converting the datatype and writing to BigQuery
     write_to_gbq = (
         data 
         | "Data Type Conversion" >> beam.ParDo(data_type_conversion())
-       # | "Write to Text" >> beam.io.WriteToText("output")
         | "Write to GBQ Table" >> beam.io.WriteToBigQuery(input_options.bigquery_table,
-        #                                                 schema=table_schema,
                                                          create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
                                                          write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
                                                          custom_gcs_temp_location='gs://chicago-crime3-batch/tmp'
